8770a07cc2fbdbbf060cc726f71f7885ed774e98
Colin Brence <cbrence@ramapo.edu>
2018-04-16 21:08:20 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,24 @@
+function forLoop([]) {
+  for (let i = 0; i< 25; i++) { 
+    if (i = 1) {
+    myArray=(`I am 1 strange loops`)
+  } else {
+    myArray=(`I am ${i} strange loops`)
+  }
+  return myArray
+}
+}
+
+function whileLoop() {
+  let n =100 ;
+  while (n >0) {
+  n--; console.log(n) }
+  return("done")
+}
+
+function doWhileLoop() {
+  do {
+    function maybeTrue() {
+      return Math.random() >= 0.5}
+  while (array.length > 0 && maybeTrue());
+}
